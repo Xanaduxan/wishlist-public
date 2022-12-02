4b7b106744940bbe7548a5353bd1212b7e7fbbdf
Anna <tima2396@yandex.ru>
2022-12-02 14:47:02 +0300

    header and footer

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Header from '../Features/Header/Header';
+import WishList from '../Features/WishList/WishList';
+import FriendsList from '../Features/FriendsList/FriendsList';
+import GroupsList from '../Features/GroupsList/GroupsList';
+import Layout from '../Features/Layout/Layout';
+import Profile from '../Features/Profile/Profile';
 
 function App():JSX.Element {
   return (
     <Routes>
-      1
+      <Route path="/" element={<Layout />}>
+      <Route path="/mywishes" element={<WishList />} />
+      <Route path="/myfriends" element={<FriendsList />} />
+      <Route path="/mygroups" element={<GroupsList />} />
+      <Route path='/profile' element={<Profile />} />
+      </Route>
     </Routes>
   );
 }
---
+++ frontend/src/Features/Profile/Profile.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Profile(): JSX.Element {
+   return (
+      <div>Profil</div>
+   );
+}
+
+export default Profile;
