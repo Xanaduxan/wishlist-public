26c4f4f6b88cf13c21e4b6612b3af3da59439093
Olga <oevkarpova@gmail.com>
2022-12-08 17:00:32 +0300

    fix

--- frontend/src/Features/GroupsList/GroupsList.tsx
+++ frontend/src/Features/GroupsList/GroupsList.tsx
@@ -3,35 +3,36 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { RootState, useAppDispatch } from '../../store';
 import { initAsyncGroups } from './groupSlice';
-import './GroupList.css'
+import './GroupList.css';
 
 function GroupsList(): JSX.Element {
    const { groups } = useSelector((state: RootState) => state.groups);
    const dispatch = useAppDispatch();
    const navigate = useNavigate();
    const myGroups = JSON.parse(JSON.stringify(groups));
-   
+
 // гит душит
    useEffect(() => {
-      dispatch(initAsyncGroups())
-    }, [])
+      dispatch(initAsyncGroups());
+    }, []);
 
    return (
-      <div className='groupList'>
+      <div className="groupList">
       <h1>Вы состоите в группах:</h1>
-         {groups.length ? groups.map((group) => 
-         <div className='groupCard'>
+         {groups.length ? groups.map((group) => (
+         <div className="groupCard">
          <div key={group.id}>
             <div>{group.name}</div>
-            <img className="groupimg" src={group.picture} alt="Groopimg"/>
+            <img className="groupimg" src={group.picture} alt="Groopimg" />
             <div>{group.description}</div>
-            <button>Выйти из группы</button>
-            </div>
+            <button className="button-add shine-button">Выйти из группы</button>
+         </div>
 
          </div>
-      ) : <div><button>Создать свою первую группу</button></ div>}
+       )
+      ) : <div><button className="button-add shine-button">Создать свою первую группу</button></div>}
       </div>
-   )
+   );
 }
 
-export default GroupsList
+export default GroupsList;
