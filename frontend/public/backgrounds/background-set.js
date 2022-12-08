c503e76399615bbb4d7ab53a076e9d8a6fd4b788
Ilya Gamayun <il.gamayun@gmail.com>
2022-12-08 10:48:44 +0300

    release 1

---
+++ frontend/public/backgrounds/background-set.js
@@ -0,0 +1,15 @@
+VANTA.BIRDS({
+    el: "#root",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: '#9fbf8e',
+    color1: '#f19351',
+    separation: 74.00,
+    alignment: 50.00,
+    backgroundAlpha: 0.70
+  })
--- frontend/src/Features/AntiWishList/AntiWishDetail.tsx
+++ frontend/src/Features/AntiWishList/AntiWishDetail.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { RootState } from '../../store';
 import './AntiWishList.css';
 
@@ -8,6 +8,7 @@ function AntiWishDetail():JSX.Element {
   const { antiWishId } = useParams();
   const { antiwishes } = useSelector((state:RootState) => state.antiwishes);
   const anti = antiwishes.find((el) => el.id === Number(antiWishId));
+  const navigate = useNavigate();
 
   return (
     <div className="container-antiwish">
@@ -16,8 +17,12 @@ function AntiWishDetail():JSX.Element {
       <div className="title-antiwish">{anti?.title}</div>
 
       <div className="description-antiwish">Описание: {anti?.description}</div>
+      <button type="button" onClick={() => navigate(-1)}>Back
+      </button>
     </div>
+
     </div>
+
   );
 }
 
